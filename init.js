da31ed70e17df1cab5cbd3a8e0e8148a54a01fde
Ankit Kumar <ankitkumar892001@gmail.com>
2021-07-13 11:35:01 +0530

    style page

--- init.js
+++ init.js
@@ -1,4 +1,4 @@
-// left-column
+// ******************** left-column ********************
 let left_col = document.querySelector(".left-col");
 let str = "";
 for (let i = 0; i < 100; i++) {
@@ -8,7 +8,7 @@ left_col.innerHTML = str;
 
 
 
-// top-row
+// ******************** top-row ********************
 let top_row = document.querySelector(".top-row");
 str = "";
 for (let i = 0; i < 26; i++) {
@@ -18,15 +18,14 @@ top_row.innerHTML = str;
 
 
 
-// 2D-array
+// ******************** grid ******************** 
 let grid = document.querySelector(".grid");
 str = "";
 for (let i = 0; i < 100; i++) {
   str += `<div class="row">`;
   for (let j = 0; j < 26; j++) {
-    str += `<div class='col' rid='${i}' cid='${j}' contenteditable='true'></div>`;
+    str += `<div class="col" rid="${i}" cid="${j}" contenteditable="true"></div>`;
   }
   str += "</div>";
 }
 grid.innerHTML = str;
-
